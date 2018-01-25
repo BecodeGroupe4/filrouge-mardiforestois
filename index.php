fae662676631e3edb9d440133ad9292f2fda6131
Marvin-LOUIS <marvinlouis@idea-box.tech>
2018-01-25 16:08:41 +0100

    Deployment modification

--- index.php
+++ index.php
@@ -1,48 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/app.css">
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-  <script src="js/jquery.js" charset="utf-8"></script>
-  <title>MILOCS</title>
-</head>
-<body>
-  <header>
-    <div class="rower">
-      <div class="logo">
-      </div>
-    </div>
-    <div class="hero">
-      <h1>Les Marchés de Bruxelles</h1>
-      <h1>Inscrivez vous a notre Newsletter</h1>
-      <div class="container">
-      <div id="timer">
-            <span id="days"></span><p>days</p>
-            <span id="hours"></span><p>hours</p>
-            <span id="minutes"></span><p>minutes</p>
-            <span id="seconds"></span><p>seconds</p>
-        </div>
-    <div>
-      <form method="post" action="index.php">
-        <label for="mail">Email: </label>
-        <input type="text" id="mail" name="mail"/>
-        <input type="submit" value="send" name="SendMail" class="send"/>
-      </form>
-    </div>
-      </div>
-    </div> 
-    <?php
+<?php
   class Newsletter {
     public $DB;
 
     function connection($host,$name,$user,$pass) {
       try {
-        $this->DB = new PDO('mysql:host='.$host.';dbname='.$name,$user,$pass);
+        $this->DB = new PDO('pgsql:host='.$host.';dbname='.$name,$user,$pass);
         $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(Exception $error) {
         die('Error : '.$error->getMessage());
@@ -66,7 +28,45 @@
     }
   }
 ?>
-  </header>
-   <script src="js/app.js"></script>
-</body>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="js/jquery.js" charset="utf-8"></script>
+    <title>Marché de Bruxelles</title>
+  </head>
+  <body>
+    <header>
+      <div class="rower">
+        <div class="logo">
+        </div>
+      </div>
+      <div class="hero">
+        <h1>Les Marchés de Bruxelles</h1>
+        <h1>Inscrivez vous a notre Newsletter</h1>
+        <div class="container">
+          <div id="timer">
+            <span id="days"></span><p>days</p>
+            <span id="hours"></span><p>hours</p>
+            <span id="minutes"></span><p>minutes</p>
+            <span id="seconds"></span><p>seconds</p>
+          </div>
+          <div>
+            <form method="post" action="index.php">
+              <label for="mail">Email: </label>
+              <input type="text" id="mail" name="mail"/>
+              <input type="submit" value="send" name="SendMail" class="send"/>
+            </form>
+          </div>
+        </div>
+      </div> 
+    </header>
+    <script src="js/app.js"></script>
+  </body>
 </html>
